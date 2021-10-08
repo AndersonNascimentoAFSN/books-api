29692bdffc0a79e0777a1d726cb44cc29e958acf
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-08 13:20:49 -0300

    feat: controller for BookController function findByPk

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -2,9 +2,9 @@ const BookService = require('../services/BookService');
 
 const findAll = async (_req, res, next) => {
   try {
-    const Book = await BookService.findAll();
+    const books = await BookService.findAll();
 
-    res.status(200).json(Book);
+    res.status(200).json(books);
   } catch (err) {
     console.log(err.message);
     next(err)
@@ -12,6 +12,21 @@ const findAll = async (_req, res, next) => {
   }
 }
 
+const findByPk = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.findByPk(id);
+
+    if(book.message) return res.status(book.code).json({message: book.message});
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+}
+
 module.exports = {
   findAll,
+  findByPk,
 }
