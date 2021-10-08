0776a7b75777af9ec1b2b48aae201edb052cacee
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-08 13:20:25 -0300

    feat: service for BookService function findByPk

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -7,6 +7,15 @@ const findAll = async () => {
   return books;
 }
 
+const findByPk = async (id) => {
+  const book = await Book.findByPk(id);
+
+  if(!book) return { code: 404, message: 'Book not found' };
+  
+  return book;
+}
+
 module.exports = {
   findAll,
+  findByPk,
 }
