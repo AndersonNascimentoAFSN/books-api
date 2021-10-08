4832cf8973c35800b928b679c00944412bcf14c6
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-08 12:49:15 -0300

    feat: create endpoint get /books

--- src/api/app.js
+++ src/api/app.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const router = require('../routes');
+const errorMiddleware = require('../middlewares/ErrorMiddleware');
+const routeNotFound = require('../middlewares/routeNotFound');
+
 const app = express();
 
 app.use(bodyParser.json());
 
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.use('/books', router.BookRoutes);
+
+app.use(routeNotFound);
+
+app.use(errorMiddleware);
 
 module.exports = app;
