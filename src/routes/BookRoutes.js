3d57b954ece7d1fa5c7c65452deada59074becdb
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-10-08 14:13:03 -0300

    feat: create endpoint put /books/:id

--- src/routes/BookRoutes.js
+++ src/routes/BookRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', BookController.findAll);
 router.get('/:id', BookController.findByPk);
 router.post('/', BookController.create);
+router.put('/:id', BookController.update);
 
 module.exports = router;
